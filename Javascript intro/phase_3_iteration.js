6389c14cd3822810c4088139e86abfeb48ff904d
tc8appdevelo <tc8appdevelo@gmail.com>
2021-11-23 16:56:46 -0600

    finished bubble sort

--- Javascript intro/phase_3_iteration.js
+++ Javascript intro/phase_3_iteration.js
@@ -1,4 +1,17 @@
 Array.prototype.bubbleSort = function() {
-    
+    let sorted = false;
+    while(!sorted) {
+        sorted = true;
+        let i = 0
+        while(i < this.length-1) {
+            if(this[i] > this[i+1]) {
+                let b = this[i+1];
+                this[i+1] = this[i];
+                this[i] = b;
+                sorted = false;
+            }
+            i++;
+        }
+    }
 
 }
